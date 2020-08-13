3f699e8de97113801b0ec8a1a87ac96ce94df979
Anthony Lepore <violacellonyc@gmail.com>
2020-08-13 20:12:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -47,24 +47,3 @@ end
 Scraper.new.print_courses
 
 
-
-
-project.css("li a data-location location-name").text
-
-project.css("project-meta.ss-icon ss-location")
-
-project.css("project-meta li a data-location location-name").text
-
-project.css("a data-location location-name").text
-
-project.css("ul.project-meta li a data-location location-name").text
-
-project.css("ul project-meta li a data-location location-name").text
-
-project.css("project-meta li a data-location location-name").text
-
-project.css("project-meta li a data-location location-name")
-
-project.css("ul.project-meta.li a data-location.span.location-name").text
-
-project.css("ul.project-meta.span.location-name").text
